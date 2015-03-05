db9614834c21b9e700fc9952d5315f03d494c415
leozukov <lad@lad-530U3BI-530U4BI-530U4BH.(none)>
2015-03-05 13:10:24 +0530

    Uploading all changes to the mvp branch. 5 march 2015 12:47 indian time

---
+++ change_password.php
@@ -0,0 +1,16 @@
+<a name="change_password"></a>
+<strong>Change password</strong>
+<br><br>
+<table>
+<tr>
+<td>Old Password</td><td><input type=text></td>
+</tr>
+<tr>
+<td>New Password</td><td><input type=text></td>
+</tr>
+<tr>
+<td>Confirm Password</td><td><input type=text></td>
+</tr>
+</table>
+<br>
+<button>Save</button>
---
+++ settings.php
@@ -0,0 +1,46 @@
+<html>
+<title>Let's meet</title>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<meta name="keywords" content="Let's meet, meet, meet-up, meet up, meetings, meeting, conversations, conversation, communication, communications" /> 
+<meta name="description" content="Let's meet - a platform for fabulous meet ups and conversations in your city" /> 
+<meta name="generator" content="LAD Software & Services UG" />
+</head>
+
+<body>
+
+<?php include ("header.php"); ?>
+
+<br><br><br><br>
+
+<?php include ("dashboard_menu.php"); ?>
+
+<br><br><br><br>
+
+<table>
+<tr>
+
+<td width=20% valign=top>Settings
+<br>
+<a href="#notifications" target="" title="">Notifications<a>
+<br>
+<a href="#change_password" target="" title="">Change password<a>
+</td>
+
+<td width=80%>
+<?php include ("notifications.php"); ?>
+<br><br>
+<?php include ("change_password.php"); ?>
+</td>
+
+</tr>
+</table>
+
+<br><br><br><br>
+<hr width=100%>
+
+<br>
+<?php include ("footer.php"); ?>
+
+</body>
+</html>
